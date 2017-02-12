152da734e880dc6ab65b1bea8153ccc8f349517b
vagrant <vagrant@homestead>
2017-02-12 15:02:06 +0000

    psql

--- config/database.php
+++ config/database.php
@@ -66,14 +66,15 @@ return [
 
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'     => env('DB_HOST', 'localhost'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host'     => $db_config['host'],
+            'database' => $db_config['database'],
+            'username' => $db_config['username'],
+            'password' => $db_config['password'],
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
         ],
+
 
         'sqlsrv' => [
             'driver'   => 'sqlsrv',
